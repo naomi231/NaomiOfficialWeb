bc49a04eb92f1b134a9a08085e9b36b0af9fbb4d
Naomi <naomi.makino@outlook.com>
2026-06-15 19:55:05 +0300

    Updates

---
+++ assets/index-IbCRaJdk.js
@@ -0,0 +1,16 @@
+(function(){let e=document.createElement(`link`).relList;if(e&&e.supports&&e.supports(`modulepreload`))return;for(let e of document.querySelectorAll(`link[rel="modulepreload"]`))n(e);new MutationObserver(e=>{for(let t of e)if(t.type===`childList`)for(let e of t.addedNodes)e.tagName===`LINK`&&e.rel===`modulepreload`&&n(e)}).observe(document,{childList:!0,subtree:!0});function t(e){let t={};return e.integrity&&(t.integrity=e.integrity),e.referrerPolicy&&(t.referrerPolicy=e.referrerPolicy),e.crossOrigin===`use-credentials`?t.credentials=`include`:e.crossOrigin===`anonymous`?t.credentials=`omit`:t.credentials=`same-origin`,t}function n(e){if(e.ep)return;e.ep=!0;let n=t(e);fetch(e.href,n)}})(),document.querySelector(`#app`).innerHTML=`
+  <div class="container">
+    <div class="glass-card">
+      <div class="animation-wrapper">
+        <div class="spinner"></div>
+      </div>
+      <h1>האתר בתהליך הקמה</h1>
+      <p>אנחנו עובדים על משהו מדהים, שווה לחכות!</p>
+      <div class="dots-container">
+        <span class="dot"></span>
+        <span class="dot"></span>
+        <span class="dot"></span>
+      </div>
+    </div>
+  </div>
+`;
